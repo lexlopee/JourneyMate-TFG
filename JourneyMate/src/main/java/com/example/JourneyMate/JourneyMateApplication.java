a013f1a810a8e39ffa8a04ff0cbc6d0694564523
alex <alexlcabr@gmail.com>
2026-01-20 18:59:22 +0100

    Proyecto añadido de Spring Boot

---
+++ JourneyMate/src/main/java/com/example/JourneyMate/JourneyMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.JourneyMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyMateApplication.class, args);
+	}
+
+}
---
+++ JourneyMate/src/test/java/com/example/JourneyMate/JourneyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JourneyMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

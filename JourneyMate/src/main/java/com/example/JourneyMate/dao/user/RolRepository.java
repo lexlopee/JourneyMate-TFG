1430e1b75f06282ba8a2dd0030c20a72443cf32b
dani1705 <dani@sge.com>
2026-01-27 11:59:16 +0100

    Dao user modificacion

--- JourneyMate/src/main/java/com/example/JourneyMate/dao/user/RolRepository.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/dao/user/RolRepository.java
@@ -1,7 +1,10 @@
-package com.example.JourneyMate.dao;
+package com.example.JourneyMate.dao.user;
 
 import com.example.JourneyMate.entity.user.RolEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RolRepository extends JpaRepository<RolEntity, Integer> {
+    Optional<RolEntity> findByNombre(String nombre);
 }
--- JourneyMate/src/main/java/com/example/JourneyMate/dao/user/TokenJWTRepository.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/dao/user/TokenJWTRepository.java
@@ -1,4 +1,4 @@
-package com.example.JourneyMate.dao;
+package com.example.JourneyMate.dao.user;
 
 import com.example.JourneyMate.entity.user.TokenJWTEntity;
 import org.springframework.data.jpa.repository.JpaRepository;

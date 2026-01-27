66c7be84cf6990e41a55a210330c6ca91c9376e7
dani1705 <dani@sge.com>
2026-01-27 12:16:06 +0100

    Mas dependecias en el pom, y mas clases en config security y cambios en SecurityConfig

---
+++ JourneyMate/src/main/java/com/example/JourneyMate/config/security/JWTUtil.java
@@ -0,0 +1,52 @@
+package com.example.JourneyMate.config.security;
+
+import com.example.JourneyMate.entity.user.UsuarioEntity;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final String SECRET = "12345678901234567890123456789012345678901234567890"; // 50 chars
+    private final long EXPIRATION = 1000 * 60 * 60 * 24; // 24 horas
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(UsuarioEntity usuario) {
+        return Jwts.builder()
+                .setSubject(usuario.getEmail())
+                .claim("id", usuario.getIdUsuario())
+                .claim("rol", usuario.getRol().getNombre())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}

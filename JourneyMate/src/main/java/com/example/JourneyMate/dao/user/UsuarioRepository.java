533fd5ec771ec8d3d7d9dfebb3361e76aecaeeaa
dani1705 <dani@sge.com>
2026-01-27 11:59:16 +0100

    Dao user modificacion

--- JourneyMate/src/main/java/com/example/JourneyMate/dao/user/UsuarioRepository.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/dao/user/UsuarioRepository.java
@@ -1,11 +1,11 @@
-package com.example.JourneyMate.dao;
+package com.example.JourneyMate.dao.user;
 
 import com.example.JourneyMate.entity.user.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UsurioRepository extends JpaRepository<UsuarioEntity, Integer> {
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
     Optional<UsuarioEntity> findByEmail(String email);
 
     boolean existsByEmail(String email);

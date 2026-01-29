229751406e573ea812f131ed877838ad104b0f4c
alex <alexlcabr@gmail.com>
2026-01-30 00:23:58 +0100

    Enlace backend-docker y creacio de clases implementadas

--- JourneyMate/src/main/java/com/example/JourneyMate/service/impl/interest/CategoriaServiceImpl.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/service/impl/interest/CategoriaServiceImpl.java
@@ -1,28 +1,37 @@
 package com.example.JourneyMate.service.impl.interest;
 
+import com.example.JourneyMate.dao.interest.CategoriaRepository;
 import com.example.JourneyMate.entity.interest.CategoriaEntity;
 import com.example.JourneyMate.service.interest.CategoriaService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class CategoriaServiceImpl implements CategoriaService {
+
+    private CategoriaRepository categoriaRepository;
+
+    public CategoriaServiceImpl(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
     @Override
     public List<CategoriaEntity> findAll() {
-        return List.of();
+        return categoriaRepository.findAll();
     }
 
     @Override
     public CategoriaEntity findById(Integer idCategoria) {
-        return null;
+        return categoriaRepository.findById(idCategoria).orElse(null);
     }
 
     @Override
     public CategoriaEntity save(CategoriaEntity categoria) {
-        return null;
+        return categoriaRepository.save(categoria);
     }
 
     @Override
     public void deleteById(Integer idCategoria) {
-
+        categoriaRepository.deleteById(idCategoria);
     }
 }

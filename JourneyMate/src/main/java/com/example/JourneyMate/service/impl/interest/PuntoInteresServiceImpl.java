d76224207e8a1952861d5344580d770db66ae702
alex <alexlcabr@gmail.com>
2026-01-30 00:23:58 +0100

    Enlace backend-docker y creacio de clases implementadas

--- JourneyMate/src/main/java/com/example/JourneyMate/service/impl/interest/PuntoInteresServiceImpl.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/service/impl/interest/PuntoInteresServiceImpl.java
@@ -1,38 +1,48 @@
 package com.example.JourneyMate.service.impl.interest;
 
+import com.example.JourneyMate.dao.interest.PuntoInteresRepository;
 import com.example.JourneyMate.entity.interest.PuntoInteresEntity;
 import com.example.JourneyMate.service.interest.PuntoInteresService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PuntoInteresServiceImpl implements PuntoInteresService {
+
+    private final PuntoInteresRepository puntoInteresRepository;
+
+    public PuntoInteresServiceImpl(PuntoInteresRepository puntoInteresRepository) {
+        this.puntoInteresRepository = puntoInteresRepository;
+    }
+
     @Override
     public List<PuntoInteresEntity> findByCiudadIgnoreCase(String ciudad) {
-        return List.of();
+        return puntoInteresRepository.findByCiudadIgnoreCase(ciudad);
     }
 
     @Override
     public List<PuntoInteresEntity> findByCategoriaIdCategoria(Integer idCategoria) {
-        return List.of();
+        return puntoInteresRepository.findByCategoriaIdCategoria(idCategoria);
     }
 
     @Override
     public List<PuntoInteresEntity> findByAll() {
-        return List.of();
+        return puntoInteresRepository.findAll();
     }
 
     @Override
     public PuntoInteresEntity findById(Integer idInteres) {
-        return null;
+        return puntoInteresRepository.findById(idInteres).orElse(null);
     }
 
     @Override
     public PuntoInteresEntity save(PuntoInteresEntity puntoInteres) {
-        return null;
+        return puntoInteresRepository.save(puntoInteres);
     }
 
     @Override
     public void deleteById(Integer idInteres) {
-
+        puntoInteresRepository.deleteById(idInteres);
     }
 }

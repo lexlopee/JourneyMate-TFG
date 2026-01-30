81fca9947cc81b300aafc340f566a49e7918cd7e
alex <alexlcabr@gmail.com>
2026-01-30 13:40:27 +0100

    Continuacion de creacion de las clases implementadas

--- JourneyMate/src/main/java/com/example/JourneyMate/dao/payment/PagoRepository.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/dao/payment/PagoRepository.java
@@ -7,5 +7,9 @@ import java.util.List;
 
 public interface PagoRepository extends JpaRepository<PagoEntity, Integer> {
     List<PagoEntity> findByReservaIdReserva(Integer idReserva);
-
+    List<PagoEntity> findByIdPago(Integer idPago);
+    PagoEntity findByIdPagoAndReserva_IdReserva(Integer idPago, Integer idReserva);
+    List<PagoEntity> findByReserva_Hotel_IdHotel(Integer idHotel);
+    List<PagoEntity> findByReserva_Hotel_IdHotelAndReserva_IdReservaAndReserva_Servicio_IdServicio(Integer idHotel, Integer idReserva, Integer idServicio);
+    void deleteByReserva_Hotel_IdHotel(Integer idHotel);
 }
--- JourneyMate/src/main/java/com/example/JourneyMate/service/impl/payment/MetodoServiceImpl.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/service/impl/payment/MetodoServiceImpl.java
@@ -1,28 +1,36 @@
 package com.example.JourneyMate.service.impl.payment;
 
+import com.example.JourneyMate.dao.payment.MetodoRepository;
 import com.example.JourneyMate.entity.payment.MetodoEntity;
 import com.example.JourneyMate.service.payment.MetodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MetodoServiceImpl implements MetodoService {
+
+    @Autowired
+    private MetodoRepository metodoRepository;
+
     @Override
     public List<MetodoEntity> findAll() {
-        return List.of();
+        return metodoRepository.findAll();
     }
 
     @Override
     public MetodoEntity findById(Integer idMetodo) {
-        return null;
+        return metodoRepository.findById(idMetodo).orElse(null);
     }
 
     @Override
     public MetodoEntity save(MetodoEntity metodoEntity) {
-        return null;
+        return metodoRepository.save(metodoEntity);
     }
 
     @Override
     public void deleteById(Integer idMetodo) {
-
+        metodoRepository.deleteById(idMetodo);
     }
 }

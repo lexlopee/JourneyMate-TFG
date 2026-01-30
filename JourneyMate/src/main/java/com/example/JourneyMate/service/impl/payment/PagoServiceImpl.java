85fdd1d473e0a81a6ffc8476282bb3ffa870b0ae
alex <alexlcabr@gmail.com>
2026-01-30 13:40:27 +0100

    Continuacion de creacion de las clases implementadas

--- JourneyMate/src/main/java/com/example/JourneyMate/service/impl/payment/PagoServiceImpl.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/service/impl/payment/PagoServiceImpl.java
@@ -1,43 +1,51 @@
 package com.example.JourneyMate.service.impl.payment;
 
+import com.example.JourneyMate.dao.payment.PagoRepository;
 import com.example.JourneyMate.entity.payment.PagoEntity;
 import com.example.JourneyMate.service.payment.PagoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PagoServiceImpl implements PagoService {
+
+    @Autowired
+    private PagoRepository pagoRepository;
+
     @Override
     public List<PagoEntity> findByReservaIdReserva(Integer idReserva) {
-        return List.of();
+        return pagoRepository.findByReservaIdReserva(idReserva);
     }
 
     @Override
     public List<PagoEntity> findById(Integer idPago) {
-        return List.of();
+        return pagoRepository.findByIdPago(idPago);
     }
 
     @Override
     public PagoEntity findById(Integer idPago, Integer idReserva) {
-        return null;
+        return pagoRepository.findByIdPagoAndReserva_IdReserva(idPago, idReserva);
     }
 
     @Override
     public List<PagoEntity> findByHotelId(Integer idHotel) {
-        return List.of();
+        return pagoRepository.findByReserva_Hotel_IdHotel(idHotel);
     }
 
     @Override
     public List<PagoEntity> findByHotelId(Integer idHotel, Integer idReserva, Integer idServicio) {
-        return List.of();
+        return pagoRepository.findByReserva_Hotel_IdHotelAndReserva_IdReservaAndReserva_Servicio_IdServicio(idHotel, idReserva, idServicio);
     }
 
     @Override
     public PagoEntity save(PagoEntity pagoEntity) {
-        return null;
+        return pagoRepository.save(pagoEntity);
     }
 
     @Override
     public void deleteByHotelId(Integer idHotel) {
-
+        pagoRepository.deleteByReserva_Hotel_IdHotel(idHotel);
     }
 }

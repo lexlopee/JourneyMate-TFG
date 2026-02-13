54eb0897d8f537bbafeff96a21f63184c37dadae
danie <danielfernandezdavila2005@gmail.com>
2026-02-13 15:03:35 +0100

    Carpeta Cors y modificacion del security config para que funcione y se pueda concectar con postamn

--- JourneyMate/src/main/java/com/example/JourneyMate/config/security/SecurityConfig.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/config/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(

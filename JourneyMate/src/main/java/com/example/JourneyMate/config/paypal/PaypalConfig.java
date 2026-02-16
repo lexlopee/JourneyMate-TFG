ea154c402ad08291a59f173275271d11ee81e0f7
marcos <mhernanmartin06@gmail.com>
2026-02-16 20:03:10 +0100

    implementación integración con PayPal para pagos y actualizaciones relacionadas

---
+++ JourneyMate/src/main/java/com/example/JourneyMate/config/paypal/PaypalConfig.java
@@ -0,0 +1,34 @@
+package com.example.JourneyMate.config.paypal;
+
+import com.paypal.base.rest.APIContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+
+    @Value("${paypal.secret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public APIContext apiContext() {
+        // Configuramos el modo (sandbox o live)
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+
+        // Creamos el contexto con nuestras credenciales
+        APIContext context = new APIContext(clientId, clientSecret, mode);
+        context.setConfigurationMap(configMap);
+        return context;
+    }
+}

9394137cb3c9cffa20d5d3541d4f2c57612bc0da
alexl <alexl@ALEX>
2026-02-17 10:33:55 +0100

    Arreglo de Menu

--- JourneyMateLauncher/JourneyMateLauncher/scripts/windows/launcher.bat
+++ JourneyMateLauncher/JourneyMateLauncher/scripts/windows/launcher.bat
@@ -2,9 +2,6 @@
 
 docker compose down --remove-orphans >nul 2>&1
 
-for /f "tokens=*" %%i in ('docker ps -q') do docker stop %%i >nul 2>&1
-for /f "tokens=*" %%i in ('docker ps -aq') do docker rm %%i >nul 2>&1
-
 taskkill /IM java.exe /F >nul 2>&1
 taskkill /IM mvn.exe /F >nul 2>&1
 

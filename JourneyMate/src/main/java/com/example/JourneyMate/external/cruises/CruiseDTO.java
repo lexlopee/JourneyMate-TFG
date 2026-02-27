a551fc56302262d3dc3f7bea7c286a0a96d8732c
marcos <mhernanmartin06@gmail.com>
2026-02-27 18:20:31 +0100

    correccion errores

--- JourneyMate/src/main/java/com/example/JourneyMate/external/cruises/CruiseDTO.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/external/cruises/CruiseDTO.java
@@ -1,8 +1,14 @@
 package com.example.JourneyMate.external.cruises;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CruiseDTO {
     private String nombreCrucero;
     private String nombreBarco;

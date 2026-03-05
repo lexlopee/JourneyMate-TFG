712bb12ee24cf5506754565e7ec59bc673481e36
alexl <alexl@ALEX>
2026-03-05 21:37:31 +0100

    Modificación de la Seguridad

--- JourneyMate/src/main/java/com/example/JourneyMate/config/security/SecurityConfig.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/config/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(cors -> {}) 
+                .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()

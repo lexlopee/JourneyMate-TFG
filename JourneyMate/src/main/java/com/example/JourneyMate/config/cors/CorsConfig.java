17f6a04f753a21c1ee2d66844c6065c60eefad65
danie <danielfernandezdavila2005@gmail.com>
2026-04-13 21:22:07 +0200

    Pasar de estado en las reservas a cancelado

--- JourneyMate/src/main/java/com/example/JourneyMate/config/cors/CorsConfig.java
+++ JourneyMate/src/main/java/com/example/JourneyMate/config/cors/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
 
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);

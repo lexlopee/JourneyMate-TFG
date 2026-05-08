c4c9d86d4d5e8e7914244f5376c12810f14e60c5
marcos <mhernanmartin06@gmail.com>
2026-05-08 22:11:06 +0200

    actualizacion detalles hoteles incluyendo el mapa

--- journeymate_mobile/ios/Runner/AppDelegate.swift
+++ journeymate_mobile/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyDGYnK9h3fWgT5io_ehszkPb2KHDtRMJrI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- journeymate_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ journeymate_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
-import printing
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
@@ -14,7 +13,6 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
